bb27dbbebd8d1dee07d7fa160d1bf1b08e4f795e
Jamie McDonnell <jamiemcdonnell@Jamies-MacBook-Pro.local>
2014-06-12 18:07:10 +0200

    adding settings panel

--- src/js/DoctoRTCWeb.js
+++ src/js/DoctoRTCWeb.js
@@ -17,9 +17,23 @@ var DoctoRTCWeb = (function() {
 		this.dom.networkTestTcp2Tcp = this.dom.find('.test.network.tcp2tcp');
 		this.dom.networkTestUdp2Udp.result = this.dom.networkTestUdp2Udp.find('.result');
 		this.dom.networkTestTcp2Tcp.result = this.dom.networkTestTcp2Tcp.find('.result');
+		
+		//add the link element
+		this.dom.settingsButton = this.dom.find('.settingsButton');
 
 		// Add the Settings widget.
 		this.settingsWidget = new DoctoRTCWeb.SettingsWidget(this.dom);
+		
+		var _settingsWidget = this.settingsWidget;
+		
+		//open settings functionality on click of settings button
+		$(this.dom.settingsButton)
+		.on("click", function(){
+			console.log("Settings clicked");
+			//$(settingsDialog).dialog( "open" );
+			_settingsWidget.open();
+			return false;
+		});
 
 		this.networkTestsSettings = {
 			udpTurn: {
--- src/js/SettingsWidget.js
+++ src/js/SettingsWidget.js
@@ -10,7 +10,68 @@
 		this.parentDom.append(DoctoRTCWeb.Html.SettingsWidget);
 
 		// this.dom is the jQuery object holding the Settings widget DOM.
-		this.dom = parentDom.find('.SettingsWidget');
+		this.dom = parentDom.find('#SettingsWidget');
+		this.dom.packetSizeSlider = this.dom.find("#packetSizeSlider");
+		this.dom.numberPacketsSlider = this.dom.find("#numberPacketsSlider");
+		this.dom.sendIntervalSlider = this.dom.find("#sendIntervalSlider");
+		this.dom.connectTimeoutSlider = this.dom.find("#connectTimeoutSlider");
+		this.dom.testTimeoutSlider = this.dom.find("#testTimeoutSlider");
+		this.dom.packetsBeforeTestSlider = this.dom.find("#packetsBeforeTestSlider");
+		
+		this.dom.settingsSlidersArray = [
+			{id:"packetSize", default:2500, min:10, max:10000, tooltip:"The size (in bytes) of each packet to be sent during each test."},
+			{id:"numberPackets", default:200, min:10, max:2000, tooltip:"Number of packets to be sent during each test."},
+			{id:"sendInterval", default:20, min:5, max:1000, tooltip:"Interval of sending packets."},
+			{id:"connectTimeout", default:6, min:2, max:30, tooltip:"Maximum time for establishing the connection with the TURN server and the DataChannel."},
+			{id:"testTimeout", default:30, min:2, max:120, tooltip:"Maximum duration of each test (it fails if it takes longer)."},
+			{id:"packetsBeforeTest", default:150, min:0, max:1000, tooltip:"Number of packets to be sent before each test in order to stabilize the packets flow across the DataChannel (first packets get a poor RTT so resulting statistics become inaccurate)."}
+		];
+		
+		var _parentDom = this.parentDom;
+		var _dom = this.dom;
+		
+		this.dom.dialog({	modal: true, 
+							autoOpen: false, 
+							width:"40%", 
+							buttons: [ { text: "Run tests", click: function() { 
+								var newOptions = {
+									packetSize: $("#packetSizeAmount").val(),
+									numPackets: $("#").val(),
+									numPreTestPackets: $("#").val(),
+									sendingInterval: $("#").val(),
+									connectTimeout: $("#").val(),
+									testTimeout: $("#").val()
+								};
+								_parentDom.networkTestsSettings.options = newOptions;
+								$(_dom).dialog( "close" ); 
+								_parentDom.run();
+						} } ] });
+								
+		var sliders = this.dom.settingsSlidersArray;
+		for(var x = 0; x< sliders.length; x++){
+			var slider = sliders[x];
+			//console.log("slider", slider, slider.id);
+			$( "#"+sliders[x].id+"Slider" ).slider({
+			  range: false,
+			  min: sliders[x].min,
+			  max: sliders[x].max,
+			  value: sliders[x].default,
+			  slide: function( event, ui ) {
+				  //console.log("slide", slider.id, ui.value, $( "#"+slider.id+"Amount" ));
+				$(this)
+				.closest("li")
+				.find("input")
+				.val( ui.value );
+			  }
+			})
+			.parent()
+			.find("i.helpNote")
+			.prop("title", slider.tooltip)
+			//.tooltip();
+			
+			$( "#"+sliders[x].id+"Amount" )
+			.val( $("#"+sliders[x].id+"Slider" ).slider( "value" ));
+		}
 
 		// Add here this.dom.xxxxx attributes to hold jQuery elements for each UI field
 		// (form fields, buttons, etc).
@@ -19,20 +80,25 @@
 	SettingsWidget.prototype.open = function() {
 		// This method must make the Settings widget visible (and make the rest of the web
 		// inactive).
+		console.log("SettingsWidget.prototype.open", this.dom);
+		this.dom.dialog( "open" );
 	};
 
 	SettingsWidget.prototype.close = function() {
 		// This method must make the Settings widget invisible (and make the rest of the web
 		// active).
+		console.log("SettingsWidget.prototype.close");
+		this.dom.dialog( "close" );
 	};
 
 	SettingsWidget.prototype.validate = function() {
-
+		console.log("SettingsWidget.prototype.validate");
 	};
 
 	SettingsWidget.prototype.get = function() {
 		// This method must return an Object with all the configuration settings, so the unique
 		// instance of DoctoRTCWeb can apply it when running the tests.
+		console.log("SettingsWidget.prototype.get");
 	};
 
 	DoctoRTCWeb.SettingsWidget = SettingsWidget;
